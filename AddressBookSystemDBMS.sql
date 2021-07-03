0a0e478eb3aa9382c3debb64eeb9216741bebdca
Rekha-15 <rekhapatil.1509@gmail.com>
2021-07-03 12:58:56 -0400

    added : creating database for AddressBook system using dbms : .

---
+++ AddressBookSystemDBMS.sql
@@ -0,0 +1,9 @@
+#Create a database using 'CREATE'command like:address_book_system.
+ CREATE DATABASE address_book_system;
+ 
+ #View a all databases.
+	SHOW databases;
+    #Use particular database using 'USE' command. 
+	USE address_book_system;                                 #Database changed
+	#View database.
+	SELECT DATABASE();
