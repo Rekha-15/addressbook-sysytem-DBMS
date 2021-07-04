74732814d03eef67108af038d5b4d1d5830ffbe7
Rekha-15 <rekhapatil.1509@gmail.com>
2021-07-04 14:55:38 -0400

    added : inserting new Contacts to Address Book : .

--- AddressBookSystemDBMS.sql
+++ AddressBookSystemDBMS.sql
@@ -18,9 +18,25 @@
     	city             VARCHAR(30) NOT NULL,			#City of Address Book.
     	state            VARCHAR(30) NOT NULL,			#State of Address Book.
     	zip              INT(6) NOT NULL,				#Zip of Address Book.
-    	phone_number     INT(13) NOT NULL,				#Phone number of Addess Book.
+    	phone_number     varchar(13) NOT NULL,				#Phone number of Addess Book.
     	email            VARCHAR(50) NOT NULL,			#Email of Address Book.
     	PRIMARY KEY(id)						#Make id primary key.
 	);
     #View the table using 'DESCRIBE'command.
 	DESCRIBE address_book;
+    
+    #Insert a data into table using 'INSERT' command.
+	INSERT INTO address_book(first_name, last_name, address, city , state , zip , phone_number, email) VALUES
+	    ('Rekha','Patil','GKW Layout', 'Bangalore', 'Kar',560058, 8095592954, 'patilrekha.1206@gmail.com'),
+		('Rahmi','Patil','RajajiNagar','Bangalore','Kar', 560057,9716277712,'rashmi123@gmail.com'),
+    	('Riya','Patil','Peenya','Bngalore', 'Kar', 560059,983734421,'riya15@gmail.com'),
+    	('Ramya','Patil','Peenya','Bangalore','kar',560059,8764727523,'ramya12@gmail.com');
+        #View a table in a table.
+	    SELECT * FROM address_book;
+        DROP TABLE address_book;
+        
+	  #Add a column in a table like:basic_pay;
+	 ALTER TABLE employee_payroll ADD basic_pay Double NOT NULL AFTER state;
+     
+	#Add a column in a table like:deduction
+	 ALTER TABLE employee_payroll ADD deduction Double NOT NULL AFTER basic_pay;
